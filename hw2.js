6941698ee50e7f6db43fab9c875ffa19ecc06b0e
2707svitlana <2707svitlana@gmail.com>
2024-10-15 22:30:03 +0300

    fix HW2

--- hw2.js
+++ hw2.js
@@ -2,7 +2,7 @@
 let num1 = prompt("Enter num1:");
 let num2 = prompt("Enter num2:");
 
-if (num1==NaN || num2==NaN) {
+if (isNaN(num1) || isNaN(num2)) {
     console.log("Enter please the numbers");
 } else {
     let result = num1 ** num2;
