4e5bd21de410a3272fbd6f26186a7efffebab6c8
2707svitlana <2707svitlana@gmail.com>
2024-10-17 08:44:01 +0300

    Fix hw2

--- hw2.js
+++ hw2.js
@@ -5,6 +5,6 @@ let num2 = prompt("Enter num2:");
 if (isNaN(num1) || isNaN(num2)) {
     console.log("Enter please the numbers");
 } else {
-    let result = num1 ** num2;
-    console.log("result "+ result);
+    let result = Math.pow(num1, num2);  
+    console.log("result " + result);
 }
